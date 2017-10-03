a46a28a9a8295f1b020dc28c2eeecc2851e3116f
Rohan Upponi <rponi70@gmail.com>
2017-10-02 22:32:57 -0700

    First push.

---
+++ src/JavaBrowser.java
@@ -0,0 +1,6 @@
+public class JavaBrowser {
+    public static void main(String[] args) {
+        //This is a project in the works. Initially opened on 10/2/2017 and estimated completed by 10/8/2017.
+        System.out.println("Testing");
+    }
+}
