893df5df6b5769ae55d3e1a30f2c9ba65dd49c9d
Rohan Upponi <rponi70@gmail.com>
2017-10-05 01:22:58 -0700

    First outline. This will be a JavaFX based application window implementing a HyperlinkListener interface to set pages based off of a given URL.

--- src/JavaBrowser.java
+++ src/JavaBrowser.java
@@ -1,6 +1,20 @@
-public class JavaBrowser {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.HyperlinkEvent;
+
+public class JavaBrowser extends Application implements HyperlinkListener{
+
     public static void main(String[] args) {
         //This is a project in the works. Initially opened on 10/2/2017 and estimated completed by 10/8/2017.
         System.out.println("Testing");
+    }
+
+    public void start(Stage browserWindow) {
+
+    }
+
+    public void hyperlinkUpdate(HyperlinkEvent linkEvent) {
+
     }
 }
