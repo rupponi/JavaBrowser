764ab5e92ce08509548c507fa715d75b8f0ea546
Rohan Upponi <rponi70@gmail.com>
2017-10-12 15:50:13 -0700

    Working into the browser. Have an idea of what to do: - Set up a main Pane with a url collection pane at the top that contains a text field to type in url. - Set up a JEditorPane to use due to apparent lack of equivalent tool in JavaFX. Used SwingNode to import. - Set up try-catch for collection of url, to collect string URL and then pass to JEditorPane to load using java.net package.

--- src/JavaBrowser.java
+++ src/JavaBrowser.java
@@ -1,20 +1,77 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.embed.swing.SwingNode;
+
+import javax.swing.JEditorPane;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
+import java.io.IOException;
+import java.net.IDN;
+
 
 public class JavaBrowser extends Application implements HyperlinkListener{
 
+    private TextField urlField;
+    private BorderPane mainPane, urlPane;
+    private Label statLabel, urlIn;
+
+    private JEditorPane editorPane;
+    private SwingNode editorNode;
+
+
+
+    public JavaBrowser() {
+        urlField = new TextField(" ");
+        urlPane = new BorderPane();
+        statLabel = new Label("");
+        urlIn = new Label("URL: ");
+        editorPane = new JEditorPane();
+        editorNode.setContent(editorPane);
+
+        urlPane.setLeft(urlIn);
+        urlPane.setCenter(urlField);
+
+        mainPane.setTop(urlPane);
+        mainPane.setCenter(editorNode);
+        mainPane.setBottom(statLabel);
+
+
+        urlField.setOnAction(new EventHandler<ActionEvent>() {
+           public void handle(ActionEvent urlSearch) {
+               String inputURL = urlSearch.getSource().toString().toLowerCase();
+               try {
+                   if (inputURL.startsWith("http://")) {
+                       inputURL = inputURL.substring(7);
+                       editorPane.setPage("http://" + IDN.toASCII(inputURL));
+                   }
+               } catch (IOException ix) {
+                   ix.printStackTrace();
+               }
+           }
+        });
+
+
+
+
+    }
+
+    public void start(Stage browserStage) {
+
+    }
+
+    public void hyperlinkUpdate(HyperlinkEvent eventUpdate) {
+
+    }
+
     public static void main(String[] args) {
-        //This is a project in the works. Initially opened on 10/2/2017 and estimated completed by 10/8/2017.
-        System.out.println("Testing");
+        launch(args);
     }
 
-    public void start(Stage browserWindow) {
-
-    }
-
-    public void hyperlinkUpdate(HyperlinkEvent linkEvent) {
-
-    }
 }
